221109d0bb97091578f2bc9d343d76d9164eb3bb
yuriypah <vasia.ostrovsky@yandex.ru>
2016-01-13 03:27:32 +0300

    ctest

--- application/views/adverts/list/listimg.php
+++ application/views/adverts/list/listimg.php
@@ -84,28 +84,33 @@ text-align: center;
 
                 $am = $arr[1]['value'] . " " . $arr[2]['value'] . ", " . $arr[0]['value'] . ", " . $arr[4]['label'] . ":" . $arr[4]['value'] . ", " . $arr[7]['label'] . ":" . $arr[7]['value'] . ", " . $arr[6]['label'] . ":" . $arr[6]['value'] . ", " . $arr[8]['label'] . ":" . $arr[8]['value'];
 
-                echo "<div class='category'>";
-                echo $am;
-                echo "</div>";
             }
-               /* if (mb_strlen($category_string) > 90) {
-                    echo "<div class='category' data-tooltip='".$category_string."'>";
-                    echo "<span style=''>" . $am  . "...</span>";
-                    echo "</div>";
-                } else {
-                    echo "<div class='category'>";
-                    echo $am;
-                    echo "</div>";
-                }*/
-                ?>
+            echo "<div class='category'>";
+            if ($advert->category_id == 9) {
+                echo $am;
+            } else {
+                echo ' ';
+            }
+
+            echo "</div>";
+            /* if (mb_strlen($category_string) > 90) {
+                 echo "<div class='category' data-tooltip='".$category_string."'>";
+                 echo "<span style=''>" . $am  . "...</span>";
+                 echo "</div>";
+             } else {
+                 echo "<div class='category'>";
+                 echo $am;
+                 echo "</div>";
+             }*/
+            ?>
 
             <div class="city">
                 <?php echo __('advert_page.label.city') . ': ' . $advert->city(); ?>
                 <?php
                 if ($advert->user->profile->type == 2) {
-                    echo " | <span style='color:gray'>" . __("profile_page.settings.label.type.company") . ":</span> <a href='/adverts?user=".$advert->user_id."'>" . $advert->user->profile->name . "</a>";
+                    echo " | <span style='color:gray'>" . __("profile_page.settings.label.type.company") . ":</span> <a href='/adverts?user=" . $advert->user_id . "'>" . $advert->user->profile->name . "</a>";
                 } else {
-                    echo " | <span style='color:gray'>" . __("profile_page.settings.label.type.private") . ":</span> <a href='/adverts?user=".$advert->user_id."' href='#'>" . $advert->user->profile->name . "</a>";
+                    echo " | <span style='color:gray'>" . __("profile_page.settings.label.type.private") . ":</span> <a href='/adverts?user=" . $advert->user_id . "' href='#'>" . $advert->user->profile->name . "</a>";
 
                 }
                 ?>
